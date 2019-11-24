b7cf9df164af94de5232c637ca412e21fb2f5111
Marijn Stollenga <marijnfs@unknown982cbcb5e12c.localdomain>
2019-11-24 15:56:43 +0100

    Added mandelbrot and CA

--- gd32v_lcd/src/main.c
+++ gd32v_lcd/src/main.c
@@ -1,12 +1,8 @@
 #include "lcd/lcd.h"
-#include "fatfs/tf_card.h"
 #include <string.h>
 
-unsigned char image[12800];
-FATFS fs;
-
 void init_uart0(void)
-{	
+{
 	/* enable GPIO clock */
     rcu_periph_clock_enable(RCU_GPIOA);
     /* enable USART clock */
@@ -32,13 +28,158 @@ void init_uart0(void)
     usart_interrupt_enable(USART0, USART_INT_RBNE);
 }
 
+
+// Code below written by Marijn Stollenga:
+
+void draw_mandel() {
+  int const width = 160;
+  int const height = 80;
+  LCD_Address_Set(0, 0, width - 1, height - 1);      //设置光标位置
+
+  for (int y = 0; y < height; ++y)
+    for (int x = 0; x < width; ++x)
+    {
+      float ca = 0.002 * (x - 80) / 80. - 0.7463;
+      float cb = 0.002 * (y - 40) / 80. + 0.1102;
+      float a = ca;
+      float b = cb;
+      int n = 0;
+      const int factor = 16;
+      for (; n < 65536 / factor; ++n) {
+        float ta = a * a - b * b;
+        if (ta > 2.0)
+          break;
+        b = cb + 2 * a * b;
+        a = ca + ta;
+
+      }
+      LCD_WR_DATA(n * 3);
+    }
+}
+
+uint16_t get_color(uint8_t r, uint8_t g, uint8_t b) {
+  return ((uint16_t)(r >> 3) << 11) + ((uint16_t)(g >> 2) << 5) + ((uint16_t)(b >> 3));
+}
+
+void draw_angle() {
+  uint16_t color = 0;
+  int const width = 160;
+  int const height = 80;
+
+  while (1) {
+    LCD_Address_Set(0, 0, width - 1, height - 1);      //设置光标位置
+    for (int y = 0; y < height; ++y)
+      for (int x = 0; x < width; ++x)
+        LCD_WR_DATA(get_color(y * 256 / height, color, x * 256 / width));
+    // delay_1ms(1);
+  }
+}
+
+void draw_1d_ca(int rule) {
+  int const width = 160;
+  int const height = 80;
+  uint8_t life[width * height];
+  uint8_t life_line[width];
+  uint8_t life_line_next[width];
+  uint8_t rules[8];
+
+  memset(life, 0, width * height);
+  memset(life_line, 0, width);
+  memset(life_line_next, 0, width);
+  memset(rules, 0, 8);
+  life_line[width / 2] = 1;
+  //read the rules
+  for (int n = 0; n < 8; ++n)
+  {
+    rules[n] = rule & 1;
+    rule >>= 1;
+  }
+
+  while (1) {
+    for (int n = 0; n < width; ++n)
+    {
+      uint8_t current_value = 0;
+
+      if (n == 0)
+        current_value = (life_line[width-1] << 2) + (life_line[0] << 1) + life_line[1];
+      else if (n == width - 1)
+        current_value = (life_line[width - 2] << 2) + (life_line[width - 1] << 1) + life_line[0];
+      else
+        current_value = (life_line[n-1] << 2) + (life_line[n] << 1) + life_line[n + 1];
+
+      life_line_next[n] = rules[current_value];
+    }
+
+    memcpy(life, life + width, (height - 1) * width);
+    memcpy(life + (height - 1) * width, life_line_next, width);
+    memcpy(life_line, life_line_next, width);
+
+    LCD_Address_Set(0, 0, width - 1, height - 1);
+    for (int n = 0; n < width * height; ++n)
+      LCD_WR_DATA(life[n] ? BLUE : BLACK);
+  }
+}
+
+
+void draw_conway_life() {
+  int const width = 160;
+  int const height = 80;
+  uint8_t life[width * height];
+  uint8_t life_next[width * height];
+  memset(life, 0, width * height);
+  memset(life_next, 0, width * height);
+  srand(123413);
+  for (int n = 0; n < width * height; ++n)
+    life[n] = (rand() % 7) == 1;
+
+  while (1) {
+    int idx = 0;
+    LCD_Address_Set(0, 0, width - 1, height - 1);      //设置光标位置
+    for (int y = 0; y < height; ++y)
+      for (int x = 0; x < width; ++x, ++idx) {
+        int top = (y == 0) ? height * width - width : -width;
+        int bottom = (y == (height - 1)) ? width - height * width : width;
+        int left = (x == 0) ? width - 1 : -1;
+        int right = (x == (width - 1)) ? 1 - width : 1;
+        int n_neighbours = life[idx + top] +
+        life[idx + bottom] +
+          life[idx + right] +
+          life[idx + left] +
+          life[idx + top + right] +
+          life[idx + top + left] +
+          life[idx + bottom + right] +
+          life[idx + bottom + left];
+        if (life[idx] && (n_neighbours == 2 || n_neighbours == 3)) {
+          life_next[idx] = 1;
+          LCD_WR_DATA(RED);
+          continue;
+        }
+        if (!life[idx] && n_neighbours == 3) {
+          life_next[idx] = 1;
+          LCD_WR_DATA(WHITE);
+          continue;
+        }
+        // if (n_neighbours == 1) {
+        //   life_next[idx] = 1;
+        //   LCD_WR_DATA(RED);
+        //   continue;
+        // }
+        life_next[idx] = 0;
+        LCD_WR_DATA(BLACK);
+      }
+
+    idx = 0;
+    memcpy(life, life_next, width * height);
+  	// for (int i = 0; i < width * height; i++)
+  		// LCD_WR_DATA(life[i] ? RED : BLACK);
+    // delay_1ms(40);
+  }
+
+
+}
+
 int main(void)
 {
-    uint8_t mount_is_ok = 1; /* 0: mount successful ; 1: mount failed */
-    int offset = 0;
-    FIL fil;
-    FRESULT fr;     /* FatFs return code */
-    UINT br;
 
     rcu_periph_clock_enable(RCU_GPIOA);
     rcu_periph_clock_enable(RCU_GPIOC);
@@ -48,77 +189,13 @@ int main(void)
     init_uart0();
 
     Lcd_Init();			// init OLED
-    LCD_Clear(WHITE);
-    BACK_COLOR=WHITE;
+    LCD_Clear(BLACK);
 
-    LEDR(1);
-    LEDG(1);
-    LEDB(1);
-
-    fr = f_mount(&fs, "", 1);
-    if (fr == 0)
-        mount_is_ok = 0;
-    else
-        mount_is_ok = 1;
-
-    if (mount_is_ok == 0)
-    {
-        while(1)
-        {
-            offset = 0;
-            fr = f_open(&fil, "logo.bin", FA_READ);
-            if (fr) printf("open error: %d!\n\r", (int)fr);
-            f_lseek(&fil, offset);
-            fr = f_read(&fil, image, sizeof(image), &br);
-            LCD_ShowPicture(0,0,159,39);
-            offset += 12800;
-            LEDB_TOG;
-            f_lseek(&fil, offset);
-            fr = f_read(&fil, image, sizeof(image), &br);
-            LCD_ShowPicture(0,40,159,79);
-            LEDB_TOG;
-            delay_1ms(1500);
-            f_close(&fil);
-
-            fr = f_open(&fil, "bmp.bin", FA_READ);
-            if (fr) printf("open error: %d!\n\r", (int)fr);
-            offset = 0;
-
-            for (int i=0; i<2189;i++)
-            {
-                fr = f_read(&fil, image, sizeof(image), &br);
-                LCD_ShowPicture(0,0,159,39);
-                offset += 12800;
-                f_lseek(&fil, offset);
-                LEDB_TOG;
-                fr = f_read(&fil, image, sizeof(image), &br);
-                LCD_ShowPicture(0,40,159,79);
-                offset += 12800;
-                f_lseek(&fil, offset);
-                LEDB_TOG;
-            }
-
-            /* Close the file */
-            f_close(&fil);
-        }
-    }
-    else
-    {
-        LCD_ShowString(24,  0, (u8 *)("no card found!"), BLACK);
-        LCD_ShowString(24, 16, (u8 *)("no card found!"), BLUE);
-        LCD_ShowString(24, 32, (u8 *)("no card found!"), BRED);
-        LCD_ShowString(24, 48, (u8 *)("no card found!"), GBLUE);
-        LCD_ShowString(24, 64, (u8 *)("no card found!"), RED);
-        while (1)
-        {
-            LEDR_TOG;
-            delay_1ms(200);
-            LEDG_TOG;
-            delay_1ms(200);
-            LEDB_TOG;
-            delay_1ms(200);
-        }
-    }
+    // draw_angle();
+    // draw_mandel();
+    // draw_conway_life();
+    // draw_1d_ca(110);
+    draw_1d_ca(150);
 }
 
 int _put_char(int ch)
